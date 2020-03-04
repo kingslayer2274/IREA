07e92936fde17c30968c3ff2b81d4881151753dd
kingslayer2274 <farsalzalam22@gmail.com>
2020-03-04 21:09:50 +0200

    new section

--- script.js
+++ script.js
@@ -33,3 +33,42 @@ $(document).ready(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  $(".fancybox-button").fancybox({
+    prevEffect: "none",
+    nextEffect: "none",
+    closeBtn: false,
+    nextClick: true,
+    helpers: {
+      title: { type: "inside" },
+      buttons: {}
+    }
+  });
+});
+
+$("body").scrollspy({ target: "#navbarNav" });
+
+// Add smooth scrolling
+$("#navbarNav a").on("click", function(e) {
+  // Check for a hash value
+  if (this.hash !== "") {
+    // Prevent default behavior
+    e.preventDefault();
+
+    // Store hash
+    const hash = this.hash;
+
+    // Animate smooth scroll
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      900,
+      function() {
+        // Add hash to URL after scroll
+        window.location.hash = hash;
+      }
+    );
+  }
+});
